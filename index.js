68d27e4d5892e15b149def9c602acdf9afe15d8f
Alexander Chao <chaoalexander45@gmail.com>
2020-11-13 04:31:30 -0700

    testing heroku

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //heroku link : https://chat-app-chao.herokuapp.com/
 
-var app = require('express')();
+var express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use(express.static('assets'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// app.get(`/user/:link/:id`, function(req, res){
-//   res.send(req.params);
-// });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
