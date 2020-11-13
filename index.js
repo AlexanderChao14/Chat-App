750930553d5b753749cdbceb048c4240d8c633ca
Alexander Chao <chaoalexander45@gmail.com>
2020-11-13 04:20:02 -0700

    adding script for heroku

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+//heroku link : https://chat-app-chao.herokuapp.com/
+
 var app = require('express')();
+const port = process.env.PORT || 3000;
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+// app.get(`/user/:link/:id`, function(req, res){
+//   res.send(req.params);
+// });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
@@ -13,5 +20,5 @@ io.on('connection', (socket) => {
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log(`Server listening on port ${port}.`);
 });
